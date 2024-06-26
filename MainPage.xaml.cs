0a0f3fd0b3c552aa9b6fc324402033743e25b5dd
Indel <Indel@DESKTOP-BEC38OO>
2024-06-26 14:13:47 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgoritmoNumerosPares
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            List<int> numerosPares = new List<int>();
+               for (int i = 0; i <= 100; i+=2)
+               {
+                   numerosPares.Add(i);
+               }
+
+               numeroListView.ItemsSource = numerosPares;
+
+        }
+
+
+       
+    }
+
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AlgoritmoNumerosPares
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
